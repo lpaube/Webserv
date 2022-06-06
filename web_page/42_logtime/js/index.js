711ff9862101cabf3911be2f297f36683b4e76bd
mafortin <mafortin@student.42quebec.com>
2022-06-06 16:12:15 -0400

    working on script

--- web_page/42_logtime/js/index.js
+++ web_page/42_logtime/js/index.js
@@ -1,1 +1,9 @@
-(function())
+
+var test = document.getElementById("get_time")
+test.onclick = function(){
+	const xhr = new XMLHttpRequest()
+	var input = document.getElementById("input")
+	xhr
+	alert(input.value)
+	
+ };
