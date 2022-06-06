3af772f13a7735010d738501f8a4550878c27652
mafortin <mafortin@student.42quebec.com>
2022-06-06 19:58:44 -0400

    working on upload page

--- web_page/42_logtime/js/index.js
+++ web_page/42_logtime/js/index.js
@@ -1,9 +1,28 @@
+/*
+https://stackoverflow.com/questions/45697176/send-simple-http-request-with-html-submit-button
+    function submit() {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                alert(xhr.response);
+            }
+        }
+        xhr.open('get', 'https://google.com', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        xhr.send();
+    }
+*/
 
-var test = document.getElementById("get_time")
+var test = document.getElementById("get_time");
 test.onclick = function(){
 	const xhr = new XMLHttpRequest()
-	var input = document.getElementById("input")
-	xhr
-	alert(input.value)
-	
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			alert("NO CONNECTION FOUND");
+		}
+	}
+	var input = document.getElementById("input");
+	xhr.open("GET", "new.html");
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send();
  };
