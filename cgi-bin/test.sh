de6b62c188c3fb4841ff6a1d5210577a612cc6db
mafortin <mafortin@student.42quebec.com>
2022-06-07 21:40:59 -0400

    added 2 script to test

--- cgi-bin/test.sh
+++ cgi-bin/test.sh
@@ -1,1 +1,4 @@
-echo "this is a test"
+#! /bin/bash
+
+echo "Content-type:text/html\r\n\r\n<html><head><title>Hello World - First CGI Program</title></head><body><h2>Hello World! This is my first CGI program</h2></body></html>"
+
