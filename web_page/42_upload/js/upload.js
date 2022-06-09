0fb80210161d7719c6eeade9542afa1feeeecf0d
mafortin <mafortin@student.42quebec.com>
2022-06-09 15:02:21 -0400

    fixed issue with bad config

--- web_page/42_upload/js/upload.js
+++ web_page/42_upload/js/upload.js
@@ -1,24 +1,16 @@
 
-
-const sendHTTPRequest = (method, url, data) => {
-	const promise = new Promise((resolve, reject) => {
-	const xhr =  new XMLHttpRequest();
-	xhr.open(method, url);
-	xhr.responseType = "json";
-	xhr.onload = () => {
-		resolve(xhr.response);
-	};
-	xhr.send(data);
-	});
-	return promise;
-};
-
+var ADD = "http://localhost:8000";
 
 const uploadFile = () => {
 	const file = document.getElementById("upload-file").files[0];
-	let req = new XMLHttpRequest();
 	let formData = new FormData();
 	formData.append("file", file);
+	fetch("ADD", { 
+		method: "POST"
+		headers: {
+			"Content-Type": "files",
+
+		}
 };
 
 (function(){
