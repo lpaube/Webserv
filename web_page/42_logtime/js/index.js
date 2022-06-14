574752a0f2f9af9e0b5e2efc15a790bec7c31452
LPAube <lpaube@hotmail.com>
2022-06-13 20:40:32 -0400

    Fixed Linux issues; now getting body

--- web_page/42_logtime/js/index.js
+++ web_page/42_logtime/js/index.js
@@ -1,29 +1,39 @@
 /*
-https://stackoverflow.com/questions/45697176/send-simple-http-request-with-html-submit-button
-    function submit() {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                alert(xhr.response);
-            }
-        }
-        xhr.open('get', 'https://google.com', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        xhr.send();
+  https://stackoverflow.com/questions/45697176/send-simple-http-request-with-html-submit-button
+function submit() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      alert(xhr.response);
     }
-*/
-
-
-(function(){
-const userinput = document.querySelector(".user_input").value;
-console.log(userinput);
-var get_time = document.getElementById("get_time");
-get_time.onclick = () =>	{
-	fetch("http://127.0.0.1:8000/cgi-bin/get_time.py", {
-		method: "GET"
-	}).then(response => response.text())
-	.then((content) => {
-		document.write(content);
-	});
+  }
+  xhr.open('get', 'https://google.com', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  xhr.send();
 }
-})();
+  */
+
+
+  (function(){
+    const userinput = document.querySelector(".user_input").value;
+    console.log(userinput);
+    var get_time = document.getElementById("get_time");
+    get_time.onclick = () =>	{
+      fetch("http://127.0.0.1:8000/cgi-bin/get_time.py", {
+        method: "GET"
+      }).then(response => response.text())
+        .then((content) => {
+          document.write(content);
+        });
+    }
+
+    var get_time = document.getElementById("test_html");
+    get_time.onclick = () =>	{
+      fetch("http://127.0.0.1:8000/web_page/42_logtime/test.html", {
+        method: "GET"
+      }).then(response => response.text())
+        .then((content) => {
+          document.write(content);
+        });
+    }
+  })();
