44c5d313d7570bf1bc0d5f5b8d98b9ce27e7047c
Marc-andre Fortin <mafortin@c1r5p8.42quebec.com>
2022-07-20 12:31:25 -0400

    changed upload script to bytes for the second time

--- web_page/42_upload/cgi-bin/upload.py
+++ web_page/42_upload/cgi-bin/upload.py
@@ -7,31 +7,31 @@ import os
 from unicodedata import name
 
 form = cgi.FieldStorage()
-data = sys.stdin.readlines()
+data = sys.stdin.buffer.read()
 
 type = os.getenv("CONTENT_TYPE")
 index = type.find("boundary=")
 index = index + 9
 boundary = type[index:]
-boundary = "--" + boundary + "--"
+boundary = bytes("--" + boundary + "--", encoding="utf-8")
 
 #filename
 i = 0
-start = data[1].find("filename=\"")
+start = data.find(b"filename=\"")
 if start > 0:
 	start += 10
-	end = data[1].find("\"", start)
-	filename = data[1][start:end]
+	end = data.find(b"\"", start)
+	filename = data[start:end]
 else:
 	filename = "default"
-path = "web_page/42_upload/upload/"
+path = b"web_page/42_upload/upload/"
 filename = path + filename
 #file type
-start = data[2].find("Content-Type:")
+start = data.find(b"Content-Type:")
 if start >= 0:
 	start += 14
-	end = data[2].find("/")
-	filetype = data[2][start:end]
+	end = data.find(b"/", start)
+	filetype = data[start:end]
 else:
 	filetype = "NULL"
 
@@ -39,20 +39,14 @@ if filetype == "text":
 	flags = "wt"
 else:
 	flags = "wb"
-f = open(filename, flags)
+strfilename = str(filename, "utf-8")
+f = open(strfilename, flags)
+
 
 #body-
-start = 4
-while True:
-	if data[start + 1].find(boundary) >= 0:
-		break
-	else:
-		if filetype == "text":
-			f.write(data[start])
-		else:
-			byte_array = bytes(data[start], encoding='utf-8')
-			f.write(byte_array)
-	start += 1
-
+start = data.find(b"\n", end)
+start += 3
+end = data.find(boundary)
+f.write(data[start:end - 2])
 f.close()
 print ("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\n\r\n")
